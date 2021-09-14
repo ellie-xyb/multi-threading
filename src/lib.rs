02d9847805ab9279b4732c43cf34a8fb9eb34b9e
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-09-15 01:44:23 +0900

    multi threaded

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,33 @@ fn counter(count: usize) -> usize {
     current_count
 }
 
+fn multi_counter(count: usize) -> usize {
+    let mut current_count = 0;
+    let num_threads = 8;
+    let mut handles = Vec::new();
+    for _ in 0..num_threads {
+        let count_per_thread = count / num_threads; 
+        let handle = std::thread::spawn(move || {
+            let mut local_count = 0;
+            for _ in 0..count_per_thread {
+                local_count += 1;
+            }
+            local_count
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        let local_count = handle.join().unwrap();
+        current_count += local_count;
+    }
+    current_count
+}
+
 #[cfg(test)]
 mod tests {
     use std::time::Instant;
 
-    use crate::counter;
+    use crate::{counter, multi_counter};
 
     #[test]
     fn it_works() {
@@ -19,6 +41,11 @@ mod tests {
         let start = Instant::now();
         let out = counter(count);
         println!("time: {}ms", start.elapsed().as_millis());
+
+        let start = Instant::now();
+        let out = multi_counter(count);
+        println!("time: {}ms", start.elapsed().as_millis());
+
         assert_eq!(out, count);
     }
 }
