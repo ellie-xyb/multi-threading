9b54ead9bc10c89158970eaca923ac91145bd9d7
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-09-15 01:10:01 +0900

    single threaded

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
+fn counter(count: usize) -> usize {
+    let mut current_count = 0;
+
+    for _ in 0..count {
+        current_count += 1;
+    }
+    current_count
+}
+
 #[cfg(test)]
 mod tests {
+    use std::time::Instant;
+
+    use crate::counter;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let count = 1_000_000;
+        let start = Instant::now();
+        let out = counter(count);
+        println!("time: {}ms", start.elapsed().as_millis());
+        assert_eq!(out, count);
     }
 }
